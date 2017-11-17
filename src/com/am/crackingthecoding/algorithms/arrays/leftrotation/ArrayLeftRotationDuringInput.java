89c5b2cda882ccfa59f53fbd30140f2ebbe2d97a
augustomarinho_m4u <augusto.marinho@m4u.com.br>
2017-11-17 14:19:54 -0200

    Implementing left rotation algorithm

---
+++ src/com/am/crackingthecoding/algorithms/arrays/leftrotation/ArrayLeftRotationDuringInput.java
@@ -0,0 +1,26 @@
+package com.am.crackingthecoding.algorithms.arrays.leftrotation;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayLeftRotationDuringInput {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt(); // Size of elements
+        int rotation = in.nextInt(); // Size of left rotation
+        int a[] = new int[size];
+
+        StringBuffer result = new StringBuffer();
+
+        for (int i = 0; i < size; i++) {
+            if (rotation > i) {
+                a[size - rotation + i] = in.nextInt();
+            } else {
+                a[i - rotation] = in.nextInt();
+            }
+        }
+
+        System.out.print(Arrays.toString(a).replaceAll("\\[|\\]|,", ""));
+    }
+}
---
+++ src/com/am/crackingthecoding/algorithms/arrays/leftrotation/ArrayLeftRotationFixedArray.java
@@ -0,0 +1,29 @@
+package com.am.crackingthecoding.algorithms.arrays.leftrotation;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayLeftRotationFixedArray {
+
+    public static void main(String[] args) {
+        int a[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int rotation = 4;
+        int size = a.length;
+
+        int firstElement = 0;
+        for (int i = 0; i < rotation + 1; i++) {
+
+            if (i > 0) {
+                firstElement = a[0];
+
+                for (int j = 0; j < size - 1; j++) {
+                    a[j] = a[j + 1];
+                }
+
+                a[size - 1] = firstElement;
+            }
+        }
+
+        System.out.print(Arrays.toString(a).replaceAll("\\[|\\]|,", ""));
+    }
+}
