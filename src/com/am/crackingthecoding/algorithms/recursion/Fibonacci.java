01a9a4617afd552a1a5770bd473cd4c9752aaccc
augustomarinho_m4u <augusto.marinho@m4u.com.br>
2017-11-19 10:52:53 -0200

    Implementing Fibonacci Serie generator

---
+++ src/com/am/crackingthecoding/algorithms/recursion/Fibonacci.java
@@ -0,0 +1,33 @@
+package com.am.crackingthecoding.algorithms.recursion;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class Fibonacci {
+
+    public BigInteger[] generateSerie(int maxDepth) {
+        BigInteger[] fibonacciSerie = new BigInteger[maxDepth];
+        return generate(fibonacciSerie, 0, maxDepth);
+    }
+
+    public BigInteger[] generate(BigInteger[] fibonacciSerie, int position, final int maxDepth) {
+
+        if (position < maxDepth) {
+            if (position >= 0 && position <= 1) {
+                fibonacciSerie[position] = BigInteger.valueOf(1);
+            } else {
+                fibonacciSerie[position] = fibonacciSerie[position - 2].add(fibonacciSerie[position - 1]);
+            }
+
+            generate(fibonacciSerie, ++position, maxDepth);
+        }
+
+        return fibonacciSerie;
+    }
+
+    public static void main(String args[]) {
+        Fibonacci fibonacci = new Fibonacci();
+        BigInteger[] serie = fibonacci.generateSerie(100);
+        System.out.println(Arrays.toString(serie));
+    }
+}
