24e62e5de9c5047e87049cf0bbeacc1f0d62f6ee
augustomarinho_m4u <augusto.marinho@m4u.com.br>
2017-11-20 17:27:41 -0200

    Implementing and studying MergeSort algorithm

---
+++ src/com/am/study/algortihms/Mergesort.java
@@ -0,0 +1,71 @@
+package com.am.study.algortihms;
+
+import java.util.Arrays;
+
+public class Mergesort {
+
+    public static int[] sort(int[] array) {
+
+        if (array.length <= 1) {
+
+            return array;
+        }
+
+        int meio = array.length / 2;
+        int[] dir = array.length % 2 == 0 ? new int[meio] : new int[meio + 1];
+        int[] esq = new int[meio];
+
+        int[] aux = new int[array.length];
+
+        for (int i = 0; i < meio; i++) {
+            esq[i] = array[i];
+        }
+
+        int auxIndex = 0;
+        for (int i = meio; i < array.length; i++) {
+            dir[auxIndex] = array[i];
+            auxIndex++;
+        }
+
+        esq = sort(esq);
+        dir = sort(dir);
+
+        aux = mergesort(esq, dir);
+
+        return aux;
+    }
+
+    static int[] mergesort(int[] esq, int[] dir) {
+        int[] aux = new int[esq.length + dir.length];
+
+        int indexDir = 0, indexEsq = 0, indexAux = 0;
+
+        while (indexEsq < esq.length || indexDir < dir.length) {
+            if (indexEsq < esq.length && indexDir < dir.length) {
+                if (esq[indexEsq] <= dir[indexDir]) {
+                    aux[indexAux] = esq[indexEsq];
+                    indexAux++;
+                    indexEsq++;
+                } else {
+                    aux[indexAux] = dir[indexDir];
+                    indexAux++;
+                    indexDir++;
+                }
+            } else if (indexEsq < esq.length) {
+                aux[indexAux] = esq[indexEsq];
+                indexAux++;
+                indexEsq++;
+            } else if (indexDir < dir.length) {
+                aux[indexAux] = dir[indexDir];
+                indexAux++;
+                indexDir++;
+            }
+        }
+        return aux;
+    }
+
+    public static void main(String args[]) {
+        int[] valores = {7, 3, 9, 1, 10, 200, 300, 0, -1};
+        System.out.println(Arrays.toString(sort(valores)));
+    }
+}
