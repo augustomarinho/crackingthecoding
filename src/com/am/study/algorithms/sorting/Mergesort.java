df8f6844cdcc3c62ffe29faf4ead0d261d7fed25
augustomarinho_m4u <augusto.marinho@m4u.com.br>
2017-11-24 20:15:56 -0200

    Updating MergeSort algorithm

--- src/com/am/study/algorithms/sorting/Mergesort.java
+++ src/com/am/study/algorithms/sorting/Mergesort.java
@@ -4,68 +4,42 @@ import java.util.Arrays;
 
 public class Mergesort {
 
-    public static int[] sort(int[] array) {
-
-        if (array.length <= 1) {
-
-            return array;
-        }
-
-        int meio = array.length / 2;
-        int[] dir = array.length % 2 == 0 ? new int[meio] : new int[meio + 1];
-        int[] esq = new int[meio];
-
-        int[] aux = new int[array.length];
-
-        for (int i = 0; i < meio; i++) {
-            esq[i] = array[i];
-        }
-
-        int auxIndex = 0;
-        for (int i = meio; i < array.length; i++) {
-            dir[auxIndex] = array[i];
-            auxIndex++;
-        }
-
-        esq = sort(esq);
-        dir = sort(dir);
-
-        aux = mergesort(esq, dir);
-
-        return aux;
+    public int[] sort(int[] vetor) {
+        int[] auxiliar = new int[vetor.length];
+        return sort(vetor, auxiliar, 0, vetor.length - 1);
     }
 
-    static int[] mergesort(int[] esq, int[] dir) {
-        int[] aux = new int[esq.length + dir.length];
-
-        int indexDir = 0, indexEsq = 0, indexAux = 0;
-
-        while (indexEsq < esq.length || indexDir < dir.length) {
-            if (indexEsq < esq.length && indexDir < dir.length) {
-                if (esq[indexEsq] <= dir[indexDir]) {
-                    aux[indexAux] = esq[indexEsq];
-                    indexAux++;
-                    indexEsq++;
-                } else {
-                    aux[indexAux] = dir[indexDir];
-                    indexAux++;
-                    indexDir++;
-                }
-            } else if (indexEsq < esq.length) {
-                aux[indexAux] = esq[indexEsq];
-                indexAux++;
-                indexEsq++;
-            } else if (indexDir < dir.length) {
-                aux[indexAux] = dir[indexDir];
-                indexAux++;
-                indexDir++;
-            }
+    private int[] sort(int[] vetor, int[] auxiliar, int inicio, int fim) {
+        if (inicio < fim) {
+            int meio = (inicio + fim) / 2;
+            sort(vetor, auxiliar, inicio, meio);
+            sort(vetor, auxiliar, meio + 1, fim);
+            merge(vetor, auxiliar, inicio, meio, fim);
         }
-        return aux;
+
+        return vetor;
+    }
+
+    private void merge(int[] vetor, int[] auxiliar, int inicio, int meio, int fim) {
+        for (int k = inicio; k <= fim; k++) {
+            auxiliar[k] = vetor[k];
+        }
+
+        int i = inicio;
+        int j = meio + 1;
+
+        for (int k = inicio; k <= fim; k++) {
+            if (i > meio) vetor[k] = auxiliar[j++];
+            else if (j > fim) vetor[k] = auxiliar[i++];
+            else if (auxiliar[i] < auxiliar[j]) vetor[k] = auxiliar[i++];
+            else vetor[k] = auxiliar[j++];
+        }
     }
 
     public static void main(String args[]) {
         int[] valores = {7, 3, 9, 1, 10, 200, 300, 0, -1};
-        System.out.println(Arrays.toString(sort(valores)));
+
+        Mergesort mergesort = new Mergesort();
+        System.out.println(Arrays.toString(mergesort.sort(valores)));
     }
 }
